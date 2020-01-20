b4e5d3af678745b08e560baa35f8f713f8f28b75
wcqrnj <cegledi.laszlo@hotmail.com>
2020-01-20 15:04:29 +0100

    deleted console.log

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,6 @@ module.exports = function(passport){
     opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
     opts.secretOrKey = config.secret;
     passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-        console.log(jwt_payload);
         User.getUserById({id: jwt_payload.sub}, (err, user) => {
             if(err) {
                 return done(err, false);
