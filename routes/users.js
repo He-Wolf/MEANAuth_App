16b75e6c6165fb37aebfec9e7e6ec19e7140145d
wcqrnj <cegledi.laszlo@hotmail.com>
2020-01-20 10:37:56 +0100

    p3 - User Model & Register

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const passport = require ("passport");
+const jwt = require ("jsonwebtoken");
+const User = require("../models/user");
 
-router.get("/register", (req, res, next)=>{
-    res.send("Register");
+router.post("/register", (req, res, next) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password,
+    });
+
+    User.addUser(newUser, (err, user)=> {
+        if(err){
+            res.json({success: false, msg: "Failed to register user"});
+        }
+        else {
+            res.json({success: true, msg: "User registered"});
+        }
+    });
 });
 
 router.post("/authenticate", (req, res, next)=>{
@@ -11,10 +28,6 @@ router.post("/authenticate", (req, res, next)=>{
 
 router.get("/profile", (req, res, next)=>{
     res.send("Profile");
-});
-
-router.get("/validate", (req, res, next)=>{
-    res.send("Validate");
 });
 
 module.exports = router;
