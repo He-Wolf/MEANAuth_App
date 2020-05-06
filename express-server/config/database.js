4272fd6aebce9a61f7d5c1787e4987d7c9e67cd3
wcqrnj <8CECEteam>
2020-05-06 14:17:39 +0200

    docker works

--- express-server/config/database.js
+++ express-server/config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: "mongodb://localhost:27017/meanauth",
+    //database: "mongodb://localhost:27017/meanauth",
+    database: "mongodb://database/meanauth",
     secret: "mysecret"
 }
